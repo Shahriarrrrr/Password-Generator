85b238b24e1fd864d1ccabfc130eda81847d2075
Shahriar Azam Khan <117385739+Shahriarrrrr@users.noreply.github.com>
2024-09-02 18:35:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,61 @@ def save_data():
     website = web_input.get()
     email = email_input.get()
     password = password_input.get()
-    if website != "" and email != "" and password != "":
-        is_ok = messagebox.askokcancel(title="Confirm", message=f"These are the details entered:\n{email}"
-                                                                f"\n Password : {password}\nIs it ok to save?")
-        if is_ok:
-            messagebox.showinfo(title="Success", message="Info Added Successfully")
-            with open("user_data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-                clear()
-
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Fields Empty!!")
     else:
-        messagebox.showinfo(title="Failed", message="Failed to Add,\n Insert details in right format")
-
+        try:
+            with open("user_data.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("user_data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+                clear()
+        else:
+            data.update(new_data)
+            with open("user_data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            clear()
 
 def clear():
     web_input.delete(0, END)
     email_input.delete(0, END)
     password_input.delete(0, END)
     web_input.focus()
+
+#-----------------------------Search-----------------------------------#
+
+def search():
+    website = web_input.get()
+    try:
+        with open("user_data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print("No file found")
+    else:
+        if website in data:
+            ##1 way to do it
+            # web_data = data[f"{website}"]
+            # email = web_data["email"]
+            # password = web_data["password"]
+
+            #Better way:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="confirm", message=f"Email:{email}\nPassword:{password}")
+            email_input.delete(0, END)
+            password_input.delete(0, END)
+            email_input.insert(0, email)
+            password_input.insert(0, password)
+        elif website not in data:
+            messagebox.showinfo(title="oops", message="Data not found")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -90,4 +127,7 @@ generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=35, command=save_data)
 add_button.grid(row=4, column=1, columnspan=2, sticky="W")
+
+search_button = Button(text="Search", width=5, command=search)
+search_button.grid(row=1, column=2, sticky="E")
 window.mainloop()
